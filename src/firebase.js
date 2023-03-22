e2ce17820019eed71cde0624df70cf16b5431edf
Waseem <waseemkhalo@gmail.com>
2023-03-22 19:39:55 -0400

    add firebase setup

--- src/firebase.js
+++ src/firebase.js
@@ -18,7 +18,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
+export const firestore = getFirestore(app);
 const analytics = getAnalytics(app);
 
 
