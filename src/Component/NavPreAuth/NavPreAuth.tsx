976586990a46307f8608d05037b1c372ae32a49d
Waseem <waseemkhalo@gmail.com>
2023-03-23 14:59:01 -0400

    add navpreauth

--- src/Component/NavPreAuth/NavPreAuth.tsx
+++ src/Component/NavPreAuth/NavPreAuth.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import logo from "../../assets/logo/logo.png"
+import logo from "../../assets/logo/logo.png";
 
 function NavPreAuth() {
   return (
     <>
-      <img src={logo}></img>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="flex sm:mx-4 md:mx-16 lg:mx-32 justify-between items-center">
+        <img className="sm:w-28 sm:h-28 md:w-48 md:h-48 lg:mr-80"src={logo}></img>
+        <div className="flex space-x-0 lg:ml-48">
+          <button className="w-20 h-10 bg-red hover:bg-transparent rounded-md">Sign Up</button>
+          <button className="w-20 h-10">Sign In</button>
+        </div>
+      </div>
     </>
   );
 }
