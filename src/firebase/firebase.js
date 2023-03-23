85ae5bffed52a6a6120d5b2840b7f322236e9538
Jonah-Hansen <tce_mc@telus.net>
2023-03-22 20:53:33 -0600

    removed firebase analytics as we are not using it for now

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 
@@ -19,6 +18,3 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const firestore = getFirestore(app);
-const analytics = getAnalytics(app);
-
-
