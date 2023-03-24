e011609908f9aa2b9a4562c31b72d4aa185f87d3
Jonah-Hansen <tce_mc@telus.net>
2023-03-24 16:31:45 -0600

    edit todo content

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React, { FormEvent, useEffect, useState } from "react";
+import React, { FormEvent, useEffect, useRef, useState } from "react";
 import NavModal from "./Component/NavModal/NavModal";
 import NavPostAuth from "./Component/NavPostAuth/NavPostAuth";
-import { addTodo, getTodos, Todo } from "./services/todoService";
+import { addTodo, editTodo, getTodos, Todo } from "./services/todoService";
 
 function App() {
 
   //* for testing todo services
-  const [newTodo, setNewTodo] = useState<string>('')
+  const [newTodo, setNewTodo] = useState<Todo['content']>('')
   const [todos, setTodos] = useState<Todo[]>([])
+  const [edit, setEdit] = useState<Todo['id']>('')
+  const editRef = useRef<HTMLInputElement>(null)
+
+  //quick render todos
+  const updateTodos = () => getTodos().then(todos => setTodos(todos))
+
   const handleNewTodo = async (e: FormEvent) => {
     e.preventDefault()
     if (newTodo) {
       await addTodo(newTodo)
+      updateTodos()
       setNewTodo('')
-      getTodos().then(todos => setTodos(todos))
     }
   }
 
+  const handleConfirmEdit = async () => {
+    if (editRef.current && editRef.current.value && edit) {
+      await editTodo(edit, editRef.current.value)
+      await updateTodos()
+      setEdit('')
+    } else console.log('edited todo cannot be blank');
+  }
+
   useEffect(() => {
-    getTodos().then(todos => setTodos(todos))
+    updateTodos()
   }, [])
   //* end of services testing
 
@@ -41,10 +55,24 @@ function App() {
           </form>
           <ul>
             {todos.sort((a, b) => a.timestamp.seconds - b.timestamp.seconds)
-              .map(todo =>
-                <li key={todo.id} >
-                  {todo.content}
-                </li>
+              .map(todo => (
+                edit === todo.id ?
+                  <li key={todo.id} className='flex justify-between w-1/2' >
+                    <input defaultValue={todo.content} ref={editRef} autoFocus />
+                    <div className="flex gap-4">
+                      <button onClick={handleConfirmEdit} >confirm</button>
+                      <button onClick={() => setEdit('')} >cancel</button>
+                    </div>
+                  </li>
+                  :
+                  <li key={todo.id} className='flex justify-between w-1/2' >
+                    {todo.content}
+                    <div className="flex gap-4">
+                      <button onClick={() => setEdit(todo.id)} >edit</button>
+                      <button >delete</button>
+                    </div>
+                  </li>
+              )
               )
             }
           </ul>
--- src/services/todoService.ts
+++ src/services/todoService.ts
@@ -1,15 +1,15 @@
-import { addDoc, collection, getDocs, Timestamp } from "firebase/firestore";
+import { addDoc, collection, doc, getDocs, Timestamp, updateDoc } from "firebase/firestore";
 import { firestore } from "../firebase/firebase";
 
 //* TODO object constructor/type definition
 export class Todo {
-  constructor(content: string) {
+  constructor(content: Todo['content']) {
     this.content = content;
   }
   content: string
   completed: boolean = false;
   timestamp: Timestamp = Timestamp.now()
-  id?: string
+  id: string = ''
 }
 
 //* returns a promise of array of all Todo objects
@@ -19,24 +19,29 @@ export const getTodos = async () => {
   return snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id } as Todo))
 }
 
-//* adds a new Todo object to the database. returns the document reference data from the db
-export const addTodo = async (todo: string) => {
+//* adds a new Todo object to the database.
+export const addTodo = async (todo: Todo['content']) => {
   const newTodo = new Todo(todo)
   try {
-    const docRef = await addDoc(collection(firestore, "todos"),
+    await addDoc(collection(firestore, "todos"),
       (({ id, ...rest }) => rest)(newTodo) //copy newTodo without id
     )
-    return docRef
-
   } catch (e) {
     console.error("Error adding document: ", e);
   }
 }
 
-export const editTodo = (id: string) => {
-
+//* edit the content of a todo
+export const editTodo = async (id: Todo['id'], content: Todo['content']) => {
+  try {
+    await updateDoc(doc(firestore, "todos", id), {
+      content: content
+    });
+  } catch (e) {
+    console.error("Error updating document: ", e);
+  }
 }
 
-export const removeTodo = (id: string) => {
+export const removeTodo = (id: Todo['id']) => {
 
 }
