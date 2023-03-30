2173158a9dd70a95999bb68109b6ae24c74d2bd7
Jonah-Hansen <tce_mc@telus.net>
2023-03-30 13:38:32 -0600

    add getUser

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,5 +1,5 @@
 import { User } from "firebase/auth";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, FirestoreDataConverter, getDoc, setDoc } from "firebase/firestore";
 import { db } from "../firebase/firebase";
 import { Todo } from "./todoService";
 
@@ -9,14 +9,9 @@ export class List {
   }
   title: string
   todos: Todo[] = []
-  order?: number
 }
 
 export class DbUser {
-  constructor(id: User['uid']) {
-    this.id = id
-  }
-  id: User['uid']
   preferences: {} = { colors: 'default' }
   lists: List[] = [
     {
@@ -30,16 +25,35 @@ export class DbUser {
   ]
 }
 
+// Firestore data converter for DbUser objects
+const userConverter: FirestoreDataConverter<DbUser> = {
+  toFirestore: ({ preferences, lists }) => {
+    return {
+      preferences,
+      lists
+    }
+  },
+  fromFirestore: (_snapshot) => {
+    return new DbUser()
+  }
+}
 
 /** @param userId User.uid from auth */
 export const addUser = async (userId: User['uid']) => {
-  const newUser = new DbUser(userId)
   try {
-    await setDoc(doc(db, "users", userId),
-      (({ id, ...rest }) => rest)(newUser) //copy newUser without id
-    )
+    const userDoc = doc(db, "users", userId).withConverter(userConverter)
+    await setDoc(userDoc, new DbUser())
   } catch (e) {
     console.log('error adding user: ', e);
+  }
+}
+
+/** @param userId User.uid from auth */
+export const getUser = async (userId: User['uid']): Promise<DbUser | undefined> => {
+  try {
+    return (await getDoc(doc(db, 'users', userId))).data() as DbUser
+  } catch (e) {
+    console.log('error retrieving user data: ', e);
   }
 }
 
