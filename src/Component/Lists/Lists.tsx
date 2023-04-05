c72c18d43f2b77b420cb00074e6cce99f5101237
Jonah-Hansen <tce_mc@telus.net>
2023-04-05 16:39:11 -0600

    new list form for testing

--- src/Component/Lists/Lists.tsx
+++ src/Component/Lists/Lists.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
-import { List as DBList, listenToLists } from '../../services/listService'
+import { FormEvent, useEffect, useState } from 'react'
+import { List as DBList, addList, listenToLists } from '../../services/listService'
 import List from "../List/List"
 
 export default function Lists() {
@@ -10,11 +10,27 @@ export default function Lists() {
     if (unsubscribe) return () => unsubscribe()
   }, [])
 
+  const handleNewList = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const target = e.target as HTMLFormElement & { list: { value: string } }
+    await addList(target.list.value)
+    target.reset()
+  }
+
   return (
-    <ul className="flex flex-wrap gap-2 ">
-      {lists?.sort((a, b) => a.order - b.order).map((list) =>
-        <List list={list} key={list.title} />
-      )}
-    </ul>
+    <>
+      <form onSubmit={handleNewList}>
+        <label>
+          <span>new list: </span>
+          <input name='list' placeholder='title' className="bg-transparent border-b-2 border-black focus:outline-none" />
+          <button>+</button>
+        </label>
+      </form>
+      <ul className="flex flex-wrap gap-2 ">
+        {lists?.sort((a, b) => a.order - b.order).map((list) =>
+          <List list={list} key={list.title} />
+        )}
+      </ul>
+    </>
   )
 }
