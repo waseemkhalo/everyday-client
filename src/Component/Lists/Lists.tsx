776514ac5d6f71827814f229d86e68f5d5d05bc6
Jonah-Hansen <tce_mc@telus.net>
2023-04-05 16:24:15 -0600

    set up snapshot listener for lists

--- src/Component/Lists/Lists.tsx
+++ src/Component/Lists/Lists.tsx
@@ -1,9 +1,15 @@
-import { useState } from 'react'
-import { List as DBList } from '../../services/listService'
+import { useEffect, useState } from 'react'
+import { List as DBList, listenToLists } from '../../services/listService'
 import List from "../List/List"
 
 export default function Lists() {
   const [lists, setLists] = useState<DBList[]>()
+
+  useEffect(() => {
+    const unsubscribe = listenToLists(setLists)
+    if (unsubscribe) return () => unsubscribe()
+  }, [])
+
   return (
     <ul className="flex flex-wrap gap-2 ">
       {lists?.sort((a, b) => a.order - b.order).map((list) =>
