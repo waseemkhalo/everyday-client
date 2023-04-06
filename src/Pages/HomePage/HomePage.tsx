44dd0420d55b4c35feed1f23ba3d310205017055
Waseem <waseemkhalo@gmail.com>
2023-04-06 14:10:17 -0400

    move app to homepage

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,47 +1,52 @@
 import { useEffect, useState } from "react";
 import Footer from "../../Component/Footer/Footer";
-import List from "../../Component/List/List";
+import Lists from "../../Component/Lists/Lists";
+import Login from "../../Component/Login/Login";
 import NavPostAuth from "../../Component/NavPostAuth/NavPostAuth";
 import NoteSection from "../../Component/NoteSection/NoteSection";
 import QuoteBox from "../../Component/QuoteBox/QuoteBox";
 import { auth } from "../../firebase/firebase";
-import { List as DBList } from '../../services/listService';
 
 function HomePage() {
 
-  const [lists, setLists] = useState<DBList[]>()
 
-  //firebase onAuthStateChanged
-  // useEffect(() => {
-  //   const unsubscribe = auth.onAuthStateChanged(async () => {
-  //     const lists = await getLists()
-  //     setLists(lists)
-  //   });
-  //   return () => unsubscribe();
-  // }, [])
+    const [loading, setLoading] = useState(true)
+    const [signedIn, setSignedIn] = useState(false)
   
-  return (
-    <div className="App">
-    <NavPostAuth />
-    <QuoteBox />
-
-    <div className="h-2 bg-black"> </div>
-    <span>Signed in as {auth.currentUser?.displayName} </span>
-    <a href="/" onClick={() => auth.signOut()}>
-      Sign-out
-    </a>
-    <div className="h-2 bg-black"> </div>
-    {lists &&
-      <ul className="flex flex-wrap gap-2 ">
-        {lists.sort((a, b) => a.order - b.order).map((list) =>
-          <List list={list} key={list.title} />
-        )}
-      </ul>
-    }
-    <NoteSection />
-    <Footer />
-  </div>
-  )
-}
+    //firebase onAuthStateChanged
+    useEffect(() => {
+      const unsubscribe = auth.onAuthStateChanged((user) => {
+        if (user) setSignedIn(true)
+        setLoading(false)
+      });
+      return () => unsubscribe();
+    }, [])
+  
+    return (
+      <div className="App">
+        {!loading &&
+          <>
+            <NavPostAuth />
+            <QuoteBox />
+            {signedIn ? (
+              <>
+                <div className="h-2 bg-black" />
+                <span>Signed in as {auth.currentUser?.displayName} </span>
+                <a href="/" onClick={() => auth.signOut()}>
+                  Sign-out
+                </a>
+                <div className="h-2 bg-black" />
+                <Lists />
+                <NoteSection />
+              </>
+            ) : (
+              <Login />
+            )}
+          </>}
+        <Footer />
+      </div>
+    );
+  }
+  
 
 export default HomePage
