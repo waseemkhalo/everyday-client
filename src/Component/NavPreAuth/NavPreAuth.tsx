3d2c41c24155def6acf887aea32d4589d1f5eaf6
Waseem <waseemkhalo@gmail.com>
2023-04-09 13:05:46 -0400

    fix auth comp position

--- src/Component/NavPreAuth/NavPreAuth.tsx
+++ src/Component/NavPreAuth/NavPreAuth.tsx
@@ -3,36 +3,43 @@ import Login from "../Login/Login";
 import { useState } from "react";
 
 function NavPreAuth() {
-
   const [openAuth, setOpenAuth] = useState(false);
 
   const handleOpenAuth = () => {
     setOpenAuth(true);
   };
 
-
   return (
-
     <>
       <div className="flex sm:mx-4 md:mx-16 lg:mx-32 justify-between items-center">
         <img
           className="sm:w-28 sm:h-28 md:w-48 md:h-48 lg:mr-80"
-          src={logo} alt="logo"
+          src={logo}
+          alt="logo"
         />
         <div className="flex space-x-0 lg:ml-48">
-          <button 
-          className="w-20 h-10 bg-lightOrange hover:bg-orange rounded-md"
-          onClick={handleOpenAuth}
+          <button
+            className="w-20 h-10 bg-lightOrange hover:bg-orange rounded-md"
+            onClick={handleOpenAuth}
           >
-            {openAuth && <Login />}
             Sign Up
           </button>
-          <button className="w-20 h-10"
-          onClick={handleOpenAuth}>
-            {openAuth && <Login />}
-            Sign In</button>
+          <button className="w-20 h-10" onClick={handleOpenAuth}>
+            Sign In
+          </button>
         </div>
       </div>
+
+      {openAuth && (
+        <div
+          className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-500 bg-opacity-50"
+          onClick={() => setOpenAuth(false)}
+        >
+          <div>
+            <Login />
+          </div>
+        </div>
+      )}
     </>
   );
 }
