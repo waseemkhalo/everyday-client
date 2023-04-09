0e6e887bf8683da9b37a6c754fb4644ee66a857f
Waseem <waseemkhalo@gmail.com>
2023-04-09 14:12:57 -0400

    phone verfication code

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,11 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, RecaptchaVerifier } from "firebase/auth";
+import {
+  getAuth,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+} from "firebase/auth";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import { getFirestore } from "firebase/firestore";
 import { addUser } from "../services/userService";
-
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -16,7 +19,7 @@ const firebaseConfig = {
   storageBucket: "project-everyday.appspot.com",
   messagingSenderId: "700852007715",
   appId: "1:700852007715:web:1e1d20d89bc0bc16c4d296",
-  measurementId: "G-ZZ514GQ2LT"
+  measurementId: "G-ZZ514GQ2LT",
 };
 
 // config FirebaseUI
@@ -26,15 +29,15 @@ export const uiConfig = {
       // User successfully signed in.
       // this is a new user, add them to the firestore users collection
       if (authResult.additionalUserInfo.isNewUser) {
-        addUser(authResult.user.uid)
+        addUser(authResult.user.uid);
       }
       // Return type determines whether we continue the redirect automatically
       // or whether we leave that to developer to handle.
       return true;
     },
   },
-  signInFlow: 'popup',
-  signInSuccessUrl: '/home',
+  signInFlow: "popup",
+  signInSuccessUrl: "/home",
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.PhoneAuthProvider.PROVIDER_ID,
@@ -42,20 +45,54 @@ export const uiConfig = {
   ],
 };
 
-
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
 // Initialize Firebase Auth
 export const auth = getAuth(app);
-window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
-  'size': 'invisible',
-  'callback': (response) => {
-    // reCAPTCHA solved, allow signInWithPhoneNumber.
-    // ...
-    onSignInSubmit();
-  }
-}, auth);
 
+// invisible reCAPTCHA (no user input) - needed for phone auth
+window.recaptchaVerifier = new RecaptchaVerifier(
+  "recaptcha-container",
+  {
+    size: "invisible",
+    callback: (response) => {
+      // reCAPTCHA solved, allow signInWithPhoneNumber.
+      // ...
+      onSignInSubmit();
+    },
+  },
+  auth
+);
+
+// sign in with phone number
+const phoneNumber = getPhoneNumberFromUserInput();
+const appVerifier = window.recaptchaVerifier;
+
+signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+  .then((confirmationResult) => {
+    // SMS sent. Prompt user to type the code from the message, then sign the
+    // user in with confirmationResult.confirm(code).
+    window.confirmationResult = confirmationResult;
+  })
+  .catch((error) => {
+    // Error; SMS not sent
+    grecaptcha.reset(window.recaptchaWidgetId);
+
+    // Or, if you haven't stored the widget ID:
+    window.recaptchaVerifier.render().then(function (widgetId) {
+      grecaptcha.reset(widgetId);
+    });
+  });
+
+// sign in the user with verfiication code
+  const code = getCodeFromUserInput();
+  confirmationResult.confirm(code).then((result) => {
+    // User signed in successfully.
+    const user = result.user;
+    // ...
+    }).catch((error) => {
+    // User couldn't sign in (bad verification code?)
+    });
+  
