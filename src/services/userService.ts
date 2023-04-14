66c74affcf9af9103d0ddd1a8716237aff8fa39a
Jonah-Hansen <tce_mc@telus.net>
2023-04-13 18:49:47 -0600

    add date, time and number 1 when creating new user

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,10 +1,17 @@
 import { User } from "firebase/auth";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase/firebase";
 import { addDefaultLists } from "./listService";
 
 /** @param userId User.uid from auth */
 export const addUser = async (userId: User['uid']) => {
   try {
     await addDefaultLists(userId)
+    await updateDoc(doc(db, 'users', userId), {
+      date: new Date().toString(),
+      number: 1,
+      time: new Date().toLocaleTimeString('en-US', { timeStyle: 'short' })
+    })
   } catch (e) {
     console.error('error adding user: ', e);
   }
