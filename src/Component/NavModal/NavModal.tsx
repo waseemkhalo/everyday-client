ad278d485a74df589319b137772c1dd709876547
Jonah-Hansen <tce_mc@telus.net>
2023-04-16 14:53:08 -0600

    nav modal styling

--- src/Component/NavModal/NavModal.tsx
+++ src/Component/NavModal/NavModal.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import { auth } from "../../firebase/firebase";
+import { useEffect, useState } from "react";
 import hamburgerMenu from "../../assets/icons/hamburger-menu.png";
 import logo from "../../assets/logo/logo.png";
+import { auth } from "../../firebase/firebase";
 
 
 function NavModal() {
@@ -9,38 +9,42 @@ function NavModal() {
 
   const toggleModal = () => setIsOpen(!isOpen);
 
+  useEffect(() => {
+    if (isOpen) document.body.style.overflow = 'hidden'
+    else document.body.style.overflow = 'auto'
+    return () => { document.body.style.overflow = 'auto' }
+  }, [isOpen])
+
   return (
     <>
       <button onClick={toggleModal}>
         <img src={hamburgerMenu} alt="hamburger menu" />
       </button>
 
-      <div
-        className={`fixed z-10 inset-0 overflow-y-auto ${isOpen ? "block" : "hidden"}`}
-      >
+      <div className={`fixed z-10 inset-0 ${isOpen ? "block" : "hidden"}`} >
         <div className="flex items-center justify-center min-h-screen">
 
           <div className="bg-white w-full h-screen rounded-md shadow-lg p-6">
 
             <button onClick={toggleModal}>
-              <h1 className='text-7xl'>X</h1>
+              <h1 className='sm:text-4xl md:text-6xl lg:text-7xl'>X</h1>
             </button>
 
-            <img src={logo} alt="logo" />
+            <img src={logo} alt="logo" className=" max-h-[40vh] m-auto" />
 
-            <ul className="flex flex-col justify-center space-y-12 items-center mb-8">
+            <ul className="text-2xl flex flex-col justify-center gap-4 items-center mb-8 ">
               <li>
-                <a className='text-3xl' href="#">Home</a>
+                <a href="/home">Home</a>
               </li>
               <li>
-                <a className='text-3xl' href="#">Feedback</a>
+                <button >Feedback</button>
               </li>
               <li>
-                <a className='text-3xl' href="#">Profile</a>
+                {/* <a className='text-3xl' href="#">Profile</a> */}
               </li>
               <li>
-                <a className='text-3xl' href="/" onClick={() => auth.signOut()}>Logout</a>
-              </li>              
+                <a href="/" onClick={() => auth.signOut()}>Logout</a>
+              </li>
             </ul>
           </div>
         </div>
