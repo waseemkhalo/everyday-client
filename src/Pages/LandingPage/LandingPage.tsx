7f345d70bd2d30cf371e7225d6b4f4a4a5d289e1
Jonah-Hansen <tce_mc@telus.net>
2023-04-16 12:32:43 -0600

    if user session exisits redirect to /home

--- src/Pages/LandingPage/LandingPage.tsx
+++ src/Pages/LandingPage/LandingPage.tsx
@@ -1,6 +1,10 @@
 import NavPreAuth from "../../Component/NavPreAuth/NavPreAuth";
+import { auth } from "../../firebase/firebase";
 
 function LandingPage() {
+  auth.onAuthStateChanged((user) => {
+    if (user) window.location.replace(window.location.href + 'home')
+  })
   return (
     <>
       <NavPreAuth />
