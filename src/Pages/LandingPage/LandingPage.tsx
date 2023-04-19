c716a62690c5d46321129ac9024e87a3d8f8c97d
Jonah-Hansen <tce_mc@telus.net>
2023-04-19 16:07:57 -0600

    trying useNavigate for redirects

--- src/Pages/LandingPage/LandingPage.tsx
+++ src/Pages/LandingPage/LandingPage.tsx
@@ -1,14 +1,16 @@
-import LandingHero from "../../Component/LandingHero/LandingHero";
-import NavPreAuth from "../../Component/NavPreAuth/NavPreAuth";
-import LandingMain from "../../Component/LandingMain/LandingMain";
-import Footer from "../../Component/Footer/Footer";
-import { auth } from "../../firebase/firebase";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Footer from "../../Component/Footer/Footer";
+import LandingHero from "../../Component/LandingHero/LandingHero";
+import LandingMain from "../../Component/LandingMain/LandingMain";
+import NavPreAuth from "../../Component/NavPreAuth/NavPreAuth";
+import { auth } from "../../firebase/firebase";
 
 function LandingPage() {
+  const navigate = useNavigate()
   // if user is already logged in, redirect to home
   auth.onAuthStateChanged((user) => {
-    if (user) window.location.replace(window.location.href + 'home')
+    if (user) navigate('/home', { replace: true })
   })
 
   const [openAuth, setOpenAuth] = useState(false);
@@ -23,8 +25,8 @@ function LandingPage() {
 
   return (
     <>
-      <NavPreAuth openAuth={openAuth} handleOpenAuth={handleOpenAuth} handleCloseAuth={handleCloseAuth}/>
-      <LandingHero handleOpenAuth={handleOpenAuth}/>
+      <NavPreAuth openAuth={openAuth} handleOpenAuth={handleOpenAuth} handleCloseAuth={handleCloseAuth} />
+      <LandingHero handleOpenAuth={handleOpenAuth} />
       <LandingMain />
       <Footer />
 
