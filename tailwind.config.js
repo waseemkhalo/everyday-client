68eddc8f9119a52b976c4d2340d707df53dc916e
Waseem <waseemkhalo@gmail.com>
2023-04-19 13:59:19 -0400

    remove darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: media, // or 'media' or 'class' or false
+  // darkMode: media, // or 'media' or 'class' or false
   theme: {
     screens: {
       sm: "320px",
