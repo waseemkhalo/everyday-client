4e5b81b6b9ce5aad1eca124c700b3d5f848154fc
Jonah-Hansen <tce_mc@telus.net>
2023-08-03 18:50:47 -0600

    fix priority list not saving

--- src/Component/List/PriorityList.tsx
+++ src/Component/List/PriorityList.tsx
@@ -25,7 +25,7 @@ export default function PriorityList({ list }: { list: List | undefined }) {
       <div className=" shadow-lg bg-red rounded-md justify-center align-middle px-4 py-2 lg:py-6">
         {list &&
           <>
-            <Droppable droppableId="priority">
+            <Droppable droppableId="priority" type='todo'>
               {(provided) => (
                 <ul
                   {...provided.droppableProps}
