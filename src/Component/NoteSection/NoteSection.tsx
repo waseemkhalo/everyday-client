175fea8ad1d3e949a62ade0eeda5a0097c34b6b4
Waseem <waseemkhalo@gmail.com>
2023-09-10 01:50:36 -0400

    remove edit mode

--- src/Component/NoteSection/NoteSection.tsx
+++ src/Component/NoteSection/NoteSection.tsx
@@ -6,7 +6,6 @@ import MarkdownPreview from '../MarkdownPreview/MarkdownPreview';
 function NoteSection({ day }: { day: Day | Today | undefined }) {
   const [notes, setNotes] = useState<Day['notes'] | Today['notes']>()
   const [saveVisible, setSaveVisible] = useState(false)
-  const [editMode, setEditMode] = useState(false);
 
 
   useEffect(() => {
@@ -15,41 +14,39 @@ function NoteSection({ day }: { day: Day | Today | undefined }) {
 
   //on notes change, display save button
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setNotes(e.currentTarget.value);
-    setSaveVisible(true);
-  };
+    setNotes(e.currentTarget.value)
+    setSaveVisible(true)
+  }
 
-  const handleSave = () => {
-    updateNotes(notes);
-    setSaveVisible(false);
-  };
-
-  if (!day) return <></>;
-
+  // wait for notes before rendering
+  if (!day) return <></>
   return (
-    <div className="bg-smoke pb-10">
-      <div className="sm:p-4 md:px-16 md:py-8 lg:px-32 lg:py-10">
+    <div className="bg-smoke pb-10" >
+      <div className="sm:p-4 md:px-16 md:py-8 lg:px-32 lg:py-10" >
         <h1 className="pb-4 flex justify-between gap-6 items-center md:font-bold">
           Notes
-          <button
-            className={`trigger-time bg-lightGrey rounded-xl px-6 py-1 font-normal ${!saveVisible ? 'hidden' : ''}`}
-            onClick={handleSave}
+          <button className={`trigger-time bg-lightGrey rounded-xl px-6 py-1 font-normal ${!saveVisible ? 'hidden' : ''}`}
+            onClick={() => {
+              updateNotes(notes)
+              setSaveVisible(false)
+            }}
           >
             Save
           </button>
         </h1>
-        {Object.hasOwn(day, 'lists') ? (
-          <div>
+        {/* if the day object has a 'lists' property, (ie. is a prev day and not today) render notes as an uneditable paragraph */}
+        {Object.hasOwn(day, 'lists') ?
+          <>
             <MarkdownPreview content={day.notes} />
-          </div>
-        ) : (
+          </>
+          :
           <textarea
             className="bg-transparent w-full h-96 resize-none lg:pt-4 outline-none text-ghostShade"
             placeholder="Write something here..."
             value={notes}
             onChange={handleChange}
           />
-        )}
+        }
       </div>
     </div>
   );
