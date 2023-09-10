5e2bada42a7d1664e469ad6826eab93b0ec7c5ce
Waseem <waseemkhalo@gmail.com>
2023-09-10 02:15:17 -0400

    remove error... didnt work

--- src/Component/MarkdownPreview/MarkdownPreview.tsx
+++ src/Component/MarkdownPreview/MarkdownPreview.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+  import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import './MarkdownPreview.scss';
 
@@ -7,16 +7,11 @@ interface MarkdownPreviewProps {
 }
 
 const MarkdownPreview: React.FC<MarkdownPreviewProps> = ({ content }) => {
-  try {
-    return (
-      <div className="markdown-preview">
-        <ReactMarkdown>{content}</ReactMarkdown>
-      </div>
-    );
-  } catch (error) {
-    console.error('Markdown rendering error:', error);
-    return <div className="markdown-preview">Error rendering Markdown</div>;
-  }
+  return (
+    <div className="markdown-preview">
+      <ReactMarkdown>{content}</ReactMarkdown>
+    </div>
+  );
 };
 
 export default MarkdownPreview;
