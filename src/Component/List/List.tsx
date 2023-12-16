c758cd8c0a495ea3f7b020a3e9ab7a8c27eca122
Waseem <waseemkhalo@gmail.com>
2023-12-16 01:39:24 -0500

    add minium width for lists, fix flexbox bug that makes every list the same length even if nothing inside

--- src/Component/List/List.tsx
+++ src/Component/List/List.tsx
@@ -32,81 +32,84 @@ function List({ list, index, removeFromListState, dropDisabled }: { removeFromLi
   return (
     <Draggable draggableId={list.title} index={index} >
       {(provided) => (
-        <li className="w-1/2 max-w-md min-w-[200px] mx-2"
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-        >
-          <div
-            className={`flex flex-col h-full list-box shadow-lg`}
+        // add parent div to fix flexbox bug
+        <div>
+          <li className="min-w-[24rem] mx-2"
+            ref={provided.innerRef}
+            {...provided.draggableProps}
           >
-            <h2 className="text-center my-4 capitalize relative">
-              <img src={dragIcon} alt="drag"
-                className="absolute top-0 left-0"
-                {...provided.dragHandleProps}
-              />
-              {list.title}
-              {/* no delete button for daily list */}
-              {list.title !== "daily" && (
-                <button
-                  className="trigger-time mb-4 absolute top-0 right-0       "
-                  onClick={() => handleDelete(list.title)}
-                >
-                  <img src={deleteIcon} alt="delete list" className="w-6" />
-                </button>
-              )}
-            </h2>
-
-            <Droppable droppableId={list.title} type='todo' isDropDisabled={dropDisabled}>
-
-              {(provided) => (
-                // overflow-y-auto
-                <ul className="max-h-[50vh] overflow-y-auto list"
-                  ref={provided.innerRef}
-                  {...provided.droppableProps}
-                >
-                  {list.todos.map((todo, todoIndex) => (
-
-
-                    edit === todoIndex ? (
-                      <EditTodo
-                        list={list}
-                        setEdit={setEdit}
-                        todo={todo}
-                        edit={edit}
-                        key={todo.content}
-                      />
-                    ) : (
-                      <Draggable draggableId={`${list.title}-${todo.content}`} index={todoIndex} key={todo.content} isDragDisabled={edit === undefined ? false : true}>
-                        {(provided) => (
-                          <TodoItem
-                            provided={provided}
-                            index={todoIndex}
-                            list={list}
-                            setEdit={setEdit}
-                            todo={todo}
-                          />
-
-                        )}
-                      </Draggable>
-                    )
-                  ))}
-                  {provided.placeholder}
-                </ul>
-              )}
-            </Droppable>
-            <form onSubmit={handleNewTodo} className="p-0 mt-auto">
-              <label className="p-2">
-                <input
-                  className="bg-transparent w-5/6 max-w-[6rem] focus:outline-none placeholder-black placeholder-opacity-50"
-                  placeholder="Add Item"
-                  name="todo"
+            <div
+              className={`flex flex-col h-full list-box shadow-lg`}
+            >
+              <h2 className="text-center my-4 capitalize relative">
+                <img src={dragIcon} alt="drag"
+                  className="absolute top-0 left-0"
+                  {...provided.dragHandleProps}
                 />
-                <button className="group-hover/edit:visible"> + </button>
+                {list.title}
+                {/* no delete button for daily list */}
+                {list.title !== "daily" && (
+                  <button
+                    className="trigger-time mb-4 absolute top-0 right-0       "
+                    onClick={() => handleDelete(list.title)}
+                  >
+                    <img src={deleteIcon} alt="delete list" className="w-6" />
+                  </button>
+                )}
+              </h2>
 
-              </label>
-            </form>
-          </div>
-        </li>
+              <Droppable droppableId={list.title} type='todo' isDropDisabled={dropDisabled}>
+
+                {(provided) => (
+                  // overflow-y-auto
+                  <ul className="max-h-[50vh] overflow-y-auto list"
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                  >
+                    {list.todos.map((todo, todoIndex) => (
+
+
+                      edit === todoIndex ? (
+                        <EditTodo
+                          list={list}
+                          setEdit={setEdit}
+                          todo={todo}
+                          edit={edit}
+                          key={todo.content}
+                        />
+                      ) : (
+                        <Draggable draggableId={`${list.title}-${todo.content}`} index={todoIndex} key={todo.content} isDragDisabled={edit === undefined ? false : true}>
+                          {(provided) => (
+                            <TodoItem
+                              provided={provided}
+                              index={todoIndex}
+                              list={list}
+                              setEdit={setEdit}
+                              todo={todo}
+                            />
+
+                          )}
+                        </Draggable>
+                      )
+                    ))}
+                    {provided.placeholder}
+                  </ul>
+                )}
+              </Droppable>
+              <form onSubmit={handleNewTodo} className="p-0 mt-auto">
+                <label className="p-2">
+                  <input
+                    className="bg-transparent w-5/6 max-w-[6rem] focus:outline-none placeholder-black placeholder-opacity-50"
+                    placeholder="Add Item"
+                    name="todo"
+                  />
+                  <button className="group-hover/edit:visible"> + </button>
+
+                </label>
+              </form>
+            </div>
+          </li>
+        </div>
       )}
     </Draggable>
   );
--- src/Component/StaticLists/StaticLists.tsx
+++ src/Component/StaticLists/StaticLists.tsx
@@ -4,20 +4,20 @@ import { List } from "../../services/listService";
 export default function StaticLists({ lists, listOrder }: { lists: List[], listOrder: string[] }) {
   return (
     <section className="sm:py-4 sm:ml-4 md:ml-16 lg:ml-32">
-      <ul className="flex gap-4 overflow-x-auto py-4 lists-section px-4">
+      <ul className="flex gap-4 py-4 lists-section px-4">
         {lists.filter(list => list.title !== 'priority').sort((a, b) => (
           listOrder ? (listOrder.indexOf(a.title) - listOrder.indexOf(b.title))
             : a.order - b.order
         )).map((list) =>
           <li key={list.title} className="w-1/2 max-w-md min-w-[200px]">
-            <div className={`flex flex-col h-full ${list.title === 'daily' ? 'bg-lightOrange' : 'bg-smoke'} rounded-md px-4 py-2 shadow-lg`}>
+            <div className='list-box'>
               <h2 className="text-center my-4 capitalize">{list.title}</h2>
               <ul className="max-h-[50vh] overflow-y-auto list">
                 {list.todos.map((todo, index) =>
                   <li key={index} className={`flex max-w-full p-2 rounded-md group/edit hover:bg-opacity-${list.title !== 'daily' && list.title !== 'priority' ? '75' : '25'} hover:bg-white`}>
                     <input
                       type="checkbox"
-                      className="form-checkbox accent-pink-500 mr-2 trigger-time"
+                      className="form-checkbox custom-checkbox mr-2 trigger-time"
                       checked={todo.completed}
                       onChange={() => { }}
                     />
@@ -32,14 +32,14 @@ export default function StaticLists({ lists, listOrder }: { lists: List[], listO
         )}
       </ul>
       <div className='sm:pr-4 md:pr-16 lg:pr-32 sm:py-6 lg:py-10'>
-        <p className="my-2 capitalize md:font-bold">Priority list</p>
-        <div className=" shadow-lg bg-red rounded-md justify-center align-middle px-4 py-2 lg:py-6">
+        <p className="my-2 capitalize md:font-bold text-white">Priority list</p>
+        <div className="priority-box">
           <ul>
             {lists.find(list => list.title === 'priority')?.todos.map((todo, index) =>
               <li key={index} className={`flex max-w-full p-2 rounded-md group/edit hover:bg-opacity-75 hover:bg-white`}>
                 <input
                   type="checkbox"
-                  className="form-checkbox accent-pink-500 mr-2 trigger-time"
+                  className="form-checkbox custom-checkbox mr-2 trigger-time"
                   checked={todo.completed}
                   onChange={() => { }}
                 />
@@ -51,6 +51,6 @@ export default function StaticLists({ lists, listOrder }: { lists: List[], listO
           </ul>
         </div>
       </div>
-    </section>
+    </section >
   )
 }
