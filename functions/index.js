df17abaeb5fda44f0f0f7e5bc34a313a2be7ad43
Waseem <waseemkhalo@gmail.com>
2023-12-20 15:33:19 -0500

    Refactor email sending functionality

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,46 @@
-// using Twilio SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: "wrimlock@gmail.com", // Change to your recipient
-  from: "waseemkhalo@gmail.com", // Change to your verified sender
-  subject: "Sending with SendGrid is Fun",
-  text: "and easy to do anywhere, even with Node.js",
-  html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-};
-sgMail
-    .send(msg)
-    .then(() => {
-      console.log("Email sent");
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+require("dotenv").config();
+
+// Initialize the Firebase Admin SDK
+admin.initializeApp();
+
+// sendgrid config
+const sendgrid = functions.config().sendgrid;
+const API_KEY = sendgrid.key;
+const TEMPLATE_ID = sendgrid.template;
+
+if (API_KEY) {
+  sgMail.setApiKey(API_KEY);
+} else {
+  console.error("API_KEY is undefined");
+}
+
+// sendgrid email template
+
+exports.sendR = functions.pubsub.schedule("every 1 minutes").onRun(async () => {
+  const getCurrentTime = () => {
+    const now = new Date(); // Get the current date
+    return now.getHours() + ":" + now.getMinutes();
+  };
+
+  const currentTime = getCurrentTime();
+  const remindersSnap = await admin.firestore().collection("reminder").get();
+
+  remindersSnap.forEach((doc) => {
+    const reminder = doc.data();
+    if (reminder.time === currentTime) {
+      const msg = {
+        to: reminder.email,
+        from: "waseemkhalo@gmail.com",
+        templateId: TEMPLATE_ID,
+      };
+      sgMail.send(msg).then(() => {
+        console.log("Reminder email sent");
+      }).catch((error) => {
+        console.error("Error sending email:", error);
+      });
+    }
+  });
+});
