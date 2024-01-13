ad49a0a65a490a3c5879a73874ff956fffed85e8
Waseem <waseemkhalo@gmail.com>
2024-01-13 12:48:38 -0500

    add producthunt embed link

--- src/Component/Footer/Footer.tsx
+++ src/Component/Footer/Footer.tsx
@@ -1,9 +1,11 @@
 
 function Footer() {
   return (
-    <div className="p-8">
-      <h1 className="text-center text-white">Everyday © 2023</h1>
-      <h1 className="text-center text-white"> v 2.0.0</h1>
+    <div className="flex justify-center p-8">
+      <div>
+        <h1 className="text-center text-white">Everyday © 2023</h1>
+        <h1 className="text-center text-white"> v 2.0.0</h1>
+      </div>
     </div>
   );
 }
--- src/Component/QuoteBox/QuoteBox.tsx
+++ src/Component/QuoteBox/QuoteBox.tsx
@@ -12,14 +12,22 @@ function QuoteBox({ date }: { date: string | undefined }) {
   }, [date]);
 
   return (
-    <div className="quote-box">
-      {quote && //conditional in case quote fails to retrieve
-        <>
-          <span>"{quote.text}"</span>
-          {/* for null author, display 'unknown' */}
-          <span> - {quote.author ? quote.author : 'unknown'}</span>
-        </>
-      }
+    <div>
+      <div className="quote-box">
+        <div className="quote-box-quote">
+          {quote && //conditional in case quote fails to retrieve
+            <>
+              <span>"{quote.text}"</span>
+              {/* for null author, display 'unknown' */}
+              <span> - {quote.author ? quote.author : 'unknown'}</span>
+            </>
+          }
+        </div>
+        <a href="https://www.producthunt.com/posts/everyday-94b25aa3-e1da-4538-b9d4-1f864650ef90?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-everyday&#0045;94b25aa3&#0045;e1da&#0045;4538&#0045;b9d4&#0045;1f864650ef90" target="_blank" rel="noreferrer">
+          <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=409456&theme=light" alt="Everyday - Get&#0032;sh&#0042;&#0037;&#0032;done&#0032;everyday | Product Hunt" style={{ width: '250px', height: '54px' }} width="250" height="54" />
+        </a>
+      </div>
+
     </div>
   );
 }
