4756fe056d934835e5d5142fe9505f7101e5ef9a
Waseem <waseemkhalo@gmail.com>
2024-01-14 14:57:56 -0500

    Add NewsroomMain component to Newsroom page

---
+++ src/Component/NewsroomMain/NewsroomMain.tsx
@@ -0,0 +1,24 @@
+// import React, { useState } from 'react';
+// import { useParams, Link } from 'react-router-dom';
+
+function NewsroomMain() {
+
+  
+
+  return (
+    <>
+      {/* Render your articles here */}
+      <div>
+        <h2>Article 1</h2>
+
+      </div>
+      <div>
+        <h2>Article 2</h2>
+
+      </div>
+    </>
+  );
+}
+
+
+export default NewsroomMain;
--- src/Pages/Newsroom/Newsroom.tsx
+++ src/Pages/Newsroom/Newsroom.tsx
@@ -1,5 +1,6 @@
 import NavPostAuth from "../../Component/NavPostAuth/NavPostAuth";
 import NewsroomHero from "../../Component/NewsroomHero/NewsroomHero";
+import NewsroomMain from "../../Component/NewsroomMain/NewsroomMain";
 
 function Newsroom() {
 
@@ -7,6 +8,7 @@ function Newsroom() {
     <div>
       <NavPostAuth />
       <NewsroomHero />
+      <NewsroomMain />
       <h1>Blog Page</h1>
       {/* Add your blog content here */}
     </div>
