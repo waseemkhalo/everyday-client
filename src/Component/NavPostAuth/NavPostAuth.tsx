6da0deba88f7979f90f45a09cfbf13138a8640a2
Waseem <waseemkhalo@gmail.com>
2024-01-16 07:44:36 -0500

    Update logo and add link to home page

--- src/Component/NavPostAuth/NavPostAuth.tsx
+++ src/Component/NavPostAuth/NavPostAuth.tsx
@@ -1,4 +1,5 @@
-import logo from "../../assets/logo/everyday-seasons-white.png";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo/everydaypaper.png";
 import NavModal from "../NavModal/NavModal";
 
 /**top home page header */
@@ -6,10 +7,12 @@ function NavPostAuth() {
   return (
     <>
       <div className="flex sm:mx-4 md:mx-16 lg:mx-32 justify-between items-center">
-        <img
-          className="sm:w-60 sm:h-34 md:w-64 md:h-40 "
-          src={logo} alt="logo"
-        />
+        <Link to='/home'>
+          <img
+            className="sm:w-40 sm:h-34 md:w-40 md:h-40 "
+            src={logo} alt="logo"
+          />
+        </Link>
 
         <div className="flex space-x-0 lg:ml-48">
           <NavModal />
